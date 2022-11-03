35c4583a23b46789736394b05904c7476d13110a
Will <willkim0608@gmail.com>
2022-11-03 13:38:52 -0700

    Add question and answer buttons

--- assets/script.js
+++ assets/script.js
@@ -3,18 +3,28 @@ var quizPage = document.querySelector("#quizPage");
 var resultPage = document.querySelector("#resultPage");
 var scoresPage = document.querySelector("#scoresPage");
 var startBtn = document.querySelector("#startBtn");
+var askQuestion = document.querySelector("#questionDiv");
+var btnA = document.querySelector("#btnA");
+var btnB = document.querySelector("#btnB");
+var btnC = document.querySelector("#btnC");
+var btnD = document.querySelector("#btnD");
 
 // Start by hiding the all pages except Start Page
 quizPage.style.visibility = 'hidden';
 resultPage.style.visibility = 'hidden';
 scoresPage.style.visibility = 'hidden';
 
-// Makes quizPage visible and startPage hidden
+// Makes quizPage visible
 function startQuiz() {
     startPage.style.visibility = 'hidden';
     resultPage.style.visibility = 'hidden';
     resultPage.style.visibility = 'hidden';
     quizPage.style.visibility = 'visible';
+    askQuestion.textContent = questions[index].question;
+    btnA;
+    btnB;
+    btnC;
+    btnD;
   }
 
   startBtn.addEventListener("click", function (event) {
