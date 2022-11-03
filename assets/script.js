404ed159b9cd9e3195069ac4752cce7fe6a3f2c3
Will <willkim0608@gmail.com>
2022-11-03 13:12:15 -0700

    Add startQuiz func

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,22 @@ var startPage = document.querySelector("#startPage");
 var quizPage = document.querySelector("#quizPage");
 var resultPage = document.querySelector("#resultPage");
 var scoresPage = document.querySelector("#scoresPage");
+var startBtn = document.querySelector("#startBtn");
 
+// Start by hiding the all pages except Start Page
 quizPage.style.visibility = 'hidden';
 resultPage.style.visibility = 'hidden';
 scoresPage.style.visibility = 'hidden';
+
+// Makes quizPage visible and startPage hidden
+function startQuiz() {
+    startPage.style.visibility = 'hidden';
+    resultPage.style.visibility = 'hidden';
+    resultPage.style.visibility = 'hidden';
+    quizPage.style.visibility = 'visible';
+  }
+
+  startBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    startQuiz();
+  });
