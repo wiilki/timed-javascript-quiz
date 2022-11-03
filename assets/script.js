e125bde3185168a3b04d6b110215a2ed32de826a
Will <willkim0608@gmail.com>
2022-11-03 13:05:28 -0700

    Hide all pages except Start Page in JS

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+var startPage = document.querySelector("#startPage");
+var quizPage = document.querySelector("#quizPage");
+var resultPage = document.querySelector("#resultPage");
+var scoresPage = document.querySelector("#scoresPage");
+
+quizPage.style.visibility = 'hidden';
+resultPage.style.visibility = 'hidden';
+scoresPage.style.visibility = 'hidden';
