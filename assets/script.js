4df86715540fa6ca52e75ef96da30d51f6e747a0
Will <willkim0608@gmail.com>
2022-11-03 13:57:18 -0700

    Made questionArray with objects

--- assets/script.js
+++ assets/script.js
@@ -9,10 +9,42 @@ var btnB = document.querySelector("#btnB");
 var btnC = document.querySelector("#btnC");
 var btnD = document.querySelector("#btnD");
 
+// Array of questions and answers
+var questionArray = [
+    {
+      question: "Question 1?",
+      choices: ["A: isWrong", "B: isRight", "C: isWrong", "D: isWrong"],
+      answer: btnB
+    },
+    {
+      question: "Question 2?",
+      choices: ["A: isRight", "B: isWrong", "C: isWrong", "D: isWrong"],
+      answer: btnA
+    },
+    {
+      question: "Question 3?",
+      choices: ["A: isWrong", "B: isRight", "C: isWrong", "D: isWrong"],
+      answer: btnB
+    },
+    {
+      question: "Question 4?",
+      choices: ["A: isWrong", "B: isWrong", "C: isRight", "D: isWrong"],
+      answer: btnC
+    },
+    {
+      question: "Question 5?",
+      choices: ["A: isWrong", "B: isWrong", "C: isWrong", "D: isRight"],
+      answer: btnD
+    },
+  ];
+
 // Start by hiding the all pages except Start Page
 quizPage.style.visibility = 'hidden';
 resultPage.style.visibility = 'hidden';
 scoresPage.style.visibility = 'hidden';
+
+// Start at index 0
+var questionIndex = 0;
 
 // Makes quizPage visible
 function startQuiz() {
@@ -20,7 +52,7 @@ function startQuiz() {
     resultPage.style.visibility = 'hidden';
     resultPage.style.visibility = 'hidden';
     quizPage.style.visibility = 'visible';
-    askQuestion.textContent = questions[index].question;
+    askQuestion.textContent = questionArray[questionIndex].question;
     btnA;
     btnB;
     btnC;
